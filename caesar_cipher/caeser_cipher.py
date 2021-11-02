9b3b1829df0afd9891881e447b7ad5c602f1b413
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-11-02 18:58:00 +0200

    finished

---
+++ caesar_cipher/caeser_cipher.py
@@ -0,0 +1,82 @@
+import nltk
+import re
+from nltk.corpus import words, names
+
+def encrypt(text,k):
+
+    if k > 25 :
+        if k % 25 == 0 :
+            k = 25
+        else :
+            k = k - (25 *(k // 25))
+    origin_k = k
+
+    new_array=[]
+    arr_text = text.split(' ')
+    for words in arr_text:
+        text_after_encrypt = ''
+        for litters in words:
+            if ord(litters) + k > 122 :
+                k = k - 26
+            text_after_encrypt += chr(ord(litters)+k)
+            k = origin_k
+        new_array.append(text_after_encrypt)
+        new_array.append(' ')
+    last_string = ''
+    last_string = last_string.join(new_array)
+    return last_string
+
+
+
+def decrypt(text,k):
+
+    if k > 25 :
+        if k % 25 == 0 :
+            k = 25
+        else :
+            k = k - (25 *(k // 25))
+    origin_k = k
+
+    new_array=[]
+    arr_text = text.split(' ')
+    for words in arr_text:
+        text_after_encrypt = ''
+        for litters in words:
+            if ord(litters) - k < 97 :
+                k =  k - 26 
+            text_after_encrypt += chr(ord(litters)-k)
+            k = origin_k
+        new_array.append(text_after_encrypt)
+        new_array.append(' ')
+    last_string = ''
+    last_string = last_string.join(new_array)
+    return last_string
+
+
+nltk.download('words', quiet=True)
+nltk.download('names', quiet=True)
+
+
+word_list = words.words()
+name_list = names.words()
+print(word_list)
+print(name_list)
+
+
+
+def count_words(text):
+    candidate_words = text.split()
+    count = 0
+    for i in candidate_words:
+        word = re.sub(r'[^A-Za-z]+','', i)
+        if word.lower() in word_list or word in name_list:
+            count += 1
+
+    return count
+
+def crack(encrypted):
+    for i in range (0,26):
+        text = decrypt(encrypted,i)
+        percentage = int(count_words(text) / len(encrypted.split()) * 100)
+        if percentage > 50:
+            return text
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,27 @@
-from caesar_cipher import __version__
+
+from caesar_cipher.caeser_cipher import (encrypt,decrypt,crack)
+import nltk
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+
+
+
+
+def test_encrypt():
+
+   expected=encrypt("hello im ashrf",25)
+   actual='gdkkn hl zrgqe'
+   assert actual==expected
+
+def test_decrepted():
+
+   expected=decrypt("gdkkn hl zrgqe",25)
+   actual='hello im ashrf'
+   assert actual==expected
+
+# def test_crack():
+
+#    expected=decrypt("gdkkn hl zrgqe")
+#    actual='hello im ashrf'
+#    assert actual==expected
